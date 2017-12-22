2c1d846d37d9a8af07f9fc69b2663f72e78de95c
Jon Gille <jon@klarity.org>
2017-12-22 09:09:04 +0100

    Put everything in the same file for simplicity

--- src/main/kotlin/io/jgille/gcp/datastore/DatastoreErrorHandling.kt
+++ src/main/kotlin/io/jgille/gcp/datastore/DatastoreErrorHandling.kt
@@ -3,8 +3,11 @@ package io.jgille.gcp.datastore
 import com.google.cloud.NoCredentials
 import com.google.cloud.ServiceOptions
 import com.google.cloud.datastore.Datastore
+import com.google.cloud.datastore.DatastoreException
 import com.google.cloud.datastore.DatastoreOptions
+import com.google.cloud.datastore.Entity
 import com.google.cloud.http.HttpTransportOptions
+import java.util.*
 
 object DatastoreService {
 
@@ -22,5 +25,31 @@ object DatastoreService {
                 .setCredentials(NoCredentials.getInstance())
         @Suppress("UsePropertyAccessSyntax")
         return builder.build().getService()
+    }
+}
+
+class EntityAlreadyExists {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val datastore = DatastoreService.setupDatastore()
+
+            val key = datastore.newKeyFactory()
+                    .setKind("test")
+                    .newKey(UUID.randomUUID().toString())
+            val entity = Entity.newBuilder(key)
+                    .set("message", "Hello world")
+                    .build()
+
+            datastore.add(entity)
+            try {
+                datastore.add(entity)
+            } catch (e: DatastoreException) {
+                e.printStackTrace()
+            }
+        }
+
     }
 }
